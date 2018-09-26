8603ed76a3880917de526679baaddaad983136e3
mob35 <toonelicious@gmail.com>
2018-09-26 13:31:47 +0700

    register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  isLinear = true; // set true for required field
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  fourthFormGroup: FormGroup;
+  fifthFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstnameCtrl: ['', Validators.required],
+      lastnameCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      emailCtrl: ['', [
+        Validators.required,
+        Validators.email,
+      ]]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      usernameCtrl: ['', Validators.required]
+    });
+    this.fourthFormGroup = this._formBuilder.group({
+      passwordCtrl: ['', Validators.required],
+      passwordconfirmCtrl: ['', Validators.required]
+    });
+    this.fifthFormGroup = this._formBuilder.group({
+      shopnameCtrl: ['', Validators.required],
+    });
+  }
+  done() {
+    
   }
 
 }
