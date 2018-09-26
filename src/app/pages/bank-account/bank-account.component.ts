ecd728e305ba1b2454bda98607b894e4b48eb5f0
Joseboom <boom.jaidee@hotmail.com>
2018-09-26 16:10:40 +0700

    so So

--- src/app/pages/bank-account/bank-account.component.ts
+++ src/app/pages/bank-account/bank-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bank-account',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BankAccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIcon(
+      'baseline-add',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/baseline-add-24px.svg')),
+      iconRegistry.addSvgIcon(
+        'baseline-payment',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/baseline-payment-24px.svg')),
+        iconRegistry.addSvgIcon(
+          'baseline-done',
+          sanitizer.bypassSecurityTrustResourceUrl('assets/baseline-done-24px.svg'));
+  }
 
   ngOnInit() {
   }
