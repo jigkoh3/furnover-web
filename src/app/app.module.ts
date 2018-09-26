6efee4e879b1c35e8ff72f272c1674e5ba2bb56a
mob35 <toonelicious@gmail.com>
2018-09-26 15:21:53 +0700

    CONFLICT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,14 +15,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BankAccountComponent } from './pages/bank-account/bank-account.component';
 import { LogisticComponent } from './pages/logistic/logistic.component';
-<<<<<<< HEAD
 import { DeliveryTimeComponent } from './pages/modals/delivery-time/delivery-time.component';
-=======
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MessageComponent } from './components/message/message';
->>>>>>> 3f329aa48b45da0bb9e2612666dab97a1f372e84
 
 @NgModule({
   declarations: [
@@ -33,13 +30,10 @@ import { MessageComponent } from './components/message/message';
     HomeComponent,
     BankAccountComponent,
     LogisticComponent,
-<<<<<<< HEAD
-    DeliveryTimeComponent
-=======
+    DeliveryTimeComponent,
     RegisterComponent,
     ProfileComponent,
     LoginComponent
->>>>>>> 3f329aa48b45da0bb9e2612666dab97a1f372e84
   ],
   imports: [
     BrowserModule,
