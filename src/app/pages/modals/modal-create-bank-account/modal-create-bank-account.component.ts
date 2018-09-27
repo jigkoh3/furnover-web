e1b9dc20bcba5bc62801132c08bdfa00040a76fe
Joseboom <boom.jaidee@hotmail.com>
2018-09-27 17:11:45 +0700

    modal

--- src/app/pages/modals/modal-create-bank-account/modal-create-bank-account.component.ts
+++ src/app/pages/modals/modal-create-bank-account/modal-create-bank-account.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { RestApiService } from 'src/app/providers/rest-api-service/rest-api.service';
+import { Constants } from 'src/app/app.constants';
 
 @Component({
   selector: 'app-modal-create-bank-account',
@@ -20,7 +22,8 @@ export class ModalCreateBankAccountComponent implements OnInit {
 
 
   constructor(
-    private _formBuilder: FormBuilder
+    private _formBuilder: FormBuilder,
+    private restApi: RestApiService
   ) { }
 
   ngOnInit() {
@@ -33,6 +36,12 @@ export class ModalCreateBankAccountComponent implements OnInit {
       bankNumber: ['', Validators.required],
       nameBank: ['', Validators.required]
     });
+    this.getBank();
   }
-
+  async getBank() {
+    let user: any = JSON.parse(window.localStorage.getItem(Constants.URL() + '@usershop'));
+    console.log(user);
+    let getbank: any = await this.restApi.get(Constants.URL() + '/api/masterbankaccount'+user._id );
+    console.log(getbank);
+  }
 }
