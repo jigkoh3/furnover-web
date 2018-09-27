ea7234d3a4d4847cb41daa18ce4726a0fb2cd4de
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-09-27 20:53:38 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { ProfileSettingComponent } from './components/profile/profile.component'
 import { ModalCompleteComponent } from './pages/modals/modal-complete/modal-complete.component';
 import { CreateProductComponent } from './pages/create-product/create-product.component';
 import { ModalInfoBankAccountComponent } from './pages/modals/modal-info-bank-account/modal-info-bank-account.component';
+import { SettingHeaderComponent } from './components/setting-header/setting-header.component';
 
 @NgModule({
   declarations: [
@@ -52,7 +53,8 @@ import { ModalInfoBankAccountComponent } from './pages/modals/modal-info-bank-ac
     ModalPrepareShippingComponent,
     ModalCompleteComponent,
     CreateProductComponent,
-    ModalInfoBankAccountComponent
+    ModalInfoBankAccountComponent,
+    SettingHeaderComponent
   ],
   entryComponents: [
     ModalCreateBankAccountComponent,
