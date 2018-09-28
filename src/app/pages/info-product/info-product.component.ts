8980e0da5df90a54ba6bbe0cd6de8455b3493046
kanomdook <kanung.2535@gmail.com>
2018-09-28 16:36:44 +0700

    [clear] clg

--- src/app/pages/info-product/info-product.component.ts
+++ src/app/pages/info-product/info-product.component.ts
@@ -19,7 +19,6 @@ export class InfoProductComponent implements OnInit {
       main: new Date().getTime(),
       sub: []
     });
-    console.log(this.mainOptions);
   }
 
   createSubOption(idx) {
@@ -28,17 +27,14 @@ export class InfoProductComponent implements OnInit {
       sub.push(new Date().getTime());
       this.mainOptions[idx].sub = sub;
     }
-    console.log(this.mainOptions);
   }
 
   delSubOption(idx, sub_id) {
-    console.log(idx, sub_id);
     const sub: Array<any> = this.mainOptions[idx].sub;
     sub.splice(sub_id, 1);
     if (sub.length < 20) {
       this.mainOptions[idx].sub = sub;
     }
-    console.log(this.mainOptions);
   }
 
   delMainOption(idx) {
@@ -46,7 +42,6 @@ export class InfoProductComponent implements OnInit {
     if (this.mainOptions.length <= 0) {
       this.isOptions = false;
     }
-    console.log(this.mainOptions);
   }
 
 
