9ef92746d614fdd5546686abbcc3525bf1f3a88b
Joseboom <boom.jaidee@hotmail.com>
2018-09-28 16:43:06 +0700

    editpassword

--- src/app/pages/setting-password/setting-password.component.ts
+++ src/app/pages/setting-password/setting-password.component.ts
@@ -5,6 +5,9 @@ import { Constants } from 'src/app/app.constants';
 import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { ModalCompleteComponent } from 'src/app/pages/modals/modal-complete/modal-complete.component';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-setting-password',
@@ -19,6 +22,9 @@ export class SettingPasswordComponent implements OnInit {
     private restApi: RestApiService,
     private dataService: DataService,
     private _formBuilder: FormBuilder,
+    private spinner: NgxSpinnerService,
+
+    public dialog: MatDialog
 
   ) { }
 
@@ -27,15 +33,31 @@ export class SettingPasswordComponent implements OnInit {
       oldPassword: ['', Validators.required],
       newPassword: ['', Validators.required],
       verifyPassword: ['', Validators.required]
-      
+
     });
   }
-async updatePassword(){
-  // console.log(this.dataPassword);
-  try {
-    await this.restApi.post(Constants.URL() + '/api/auth/change-password',this.dataPassword);
-  } catch (error) {
-   
+  async updatePassword() {
+    // console.log(this.dataPassword);
+    this.spinner.show();
+
+    try {
+      await this.restApi.post(Constants.URL() + '/api/auth/change-password', this.dataPassword);
+      this.spinner.hide();
+      this.dialog.closeAll();
+
+      this.dialog.open(ModalCompleteComponent, {
+        width: '700px',
+        data: { message: 'แก้ไขรหัสผ่านสำเร็จ' }
+      });
+    } catch (error) {
+      this.spinner.hide();      
+      if (error) {
+        if (error['error']['message'] === 'Username or Password is invalid.') {
+          this.dataService.error('รหัสผ่านปัจจุบันไม่ถูกต้อง');
+        } else if (error['error']['message'] === 'Please verify your password.') {
+          this.dataService.error('ยืนยันรหัสผ่านไม่ถูกต้อง');
+        }
+      }
+    }
   }
-}
 }
