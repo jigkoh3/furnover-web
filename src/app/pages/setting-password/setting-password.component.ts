400b1ec67f1a8d3464f76a3c4977a48b10ff8d2f
Joseboom <boom.jaidee@hotmail.com>
2018-09-28 16:27:23 +0700

    setting pass

--- src/app/pages/setting-password/setting-password.component.ts
+++ src/app/pages/setting-password/setting-password.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from 'src/app/providers/rest-api-service/rest-api.service';
+import { DataService } from 'src/app/providers/data-service/data.service';
+import { Constants } from 'src/app/app.constants';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-setting-password',
@@ -6,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting-password.component.css']
 })
 export class SettingPasswordComponent implements OnInit {
+  dataPassword: any = {};
+  settingPassword: FormGroup;
 
-  constructor() { }
+  constructor(
+    private restApi: RestApiService,
+    private dataService: DataService,
+    private _formBuilder: FormBuilder,
+
+  ) { }
 
   ngOnInit() {
+    this.settingPassword = this._formBuilder.group({
+      oldPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      verifyPassword: ['', Validators.required]
+      
+    });
   }
-
+async updatePassword(){
+  // console.log(this.dataPassword);
+  try {
+    await this.restApi.post(Constants.URL() + '/api/auth/change-password',this.dataPassword);
+  } catch (error) {
+   
+  }
+}
 }
