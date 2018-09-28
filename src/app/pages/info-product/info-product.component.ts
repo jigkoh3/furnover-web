024847e0fdeb9e07c19f8f1be3849e0c2f355244
kanomdook <kanung.2535@gmail.com>
2018-09-28 16:36:06 +0700

    [edit] sub options

--- src/app/pages/info-product/info-product.component.ts
+++ src/app/pages/info-product/info-product.component.ts
@@ -15,11 +15,38 @@ export class InfoProductComponent implements OnInit {
 
   createOptions() {
     this.isOptions = true;
-    this.mainOptions.push(this.mainOptions.length);
+    this.mainOptions.push({
+      main: new Date().getTime(),
+      sub: []
+    });
+    console.log(this.mainOptions);
+  }
+
+  createSubOption(idx) {
+    const sub: Array<any> = this.mainOptions[idx].sub;
+    if (sub.length < 20) {
+      sub.push(new Date().getTime());
+      this.mainOptions[idx].sub = sub;
+    }
+    console.log(this.mainOptions);
+  }
+
+  delSubOption(idx, sub_id) {
+    console.log(idx, sub_id);
+    const sub: Array<any> = this.mainOptions[idx].sub;
+    sub.splice(sub_id, 1);
+    if (sub.length < 20) {
+      this.mainOptions[idx].sub = sub;
+    }
+    console.log(this.mainOptions);
   }
 
   delMainOption(idx) {
     this.mainOptions.splice(idx, 1);
+    if (this.mainOptions.length <= 0) {
+      this.isOptions = false;
+    }
+    console.log(this.mainOptions);
   }
 
 
