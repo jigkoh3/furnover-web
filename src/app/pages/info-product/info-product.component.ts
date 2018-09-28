9f4a107d8549152ce38155a654e5333ef4ab7b9b
kanomdook <kanung.2535@gmail.com>
2018-09-28 15:43:58 +0700

    [edit] option in product

--- src/app/pages/info-product/info-product.component.ts
+++ src/app/pages/info-product/info-product.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info-product.component.css']
 })
 export class InfoProductComponent implements OnInit {
-
+  isOptions = false;
+  mainOptions: Array<any> = [];
   constructor() { }
 
   ngOnInit() {
   }
+
+  createOptions() {
+    this.isOptions = true;
+    this.mainOptions.push(this.mainOptions.length);
+  }
+
+  delMainOption(idx) {
+    this.mainOptions.splice(idx, 1);
+  }
+
+
 
 }
