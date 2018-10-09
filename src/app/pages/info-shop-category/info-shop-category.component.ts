dc18c60622e4e2c6d40c49d34d09ba8ec4f5ab69
Joseboom <boom.jaidee@hotmail.com>
2018-10-09 16:43:40 +0700

    edit

--- src/app/pages/info-shop-category/info-shop-category.component.ts
+++ src/app/pages/info-shop-category/info-shop-category.component.ts
@@ -9,218 +9,239 @@ import { DomSanitizer } from "@angular/platform-browser";
 import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: "app-info-shop-category",
-  templateUrl: "./info-shop-category.component.html",
-  styleUrls: ["./info-shop-category.component.css"]
+    selector: "app-info-shop-category",
+    templateUrl: "./info-shop-category.component.html",
+    styleUrls: ["./info-shop-category.component.css"]
 })
 export class InfoShopCategoryComponent implements OnInit {
-  categoryData: any = {
-    name: "กำหนดชื่อหมวดหมู่เอง",
-    items: [],
-    status: false
-  };
-  categoryCompele: boolean = false;
-  isCheckbtnSaveProduct: boolean = false;
-  itemSeleted: Array<any> = [];
-  getProduct: Array<any> = [];
-  isEdit: boolean = false;
-  page: any = {
-    shop_id: "",
-    status: "all",
-    name: "",
-    page: 1,
-    limit: 30
-  };
-  _formBuilder: any;
-  constructor(
-    public iconRegistry: MatIconRegistry,
-    public sanitizer: DomSanitizer,
-    private restApi: RestApiService,
-    private actRoute: ActivatedRoute,
-    private spinner: NgxSpinnerService,
-    private router: Router
-  ) {
-    iconRegistry.addSvgIcon(
-      "done",
-      sanitizer.bypassSecurityTrustResourceUrl("assets/baseline-done-24px.svg")
-    );
-  }
-
-  ngOnInit() {
-    this.actRoute.queryParams.subscribe(params => {
-      console.log(params);
-      if (params["cateID"]) {
-        this.isEdit = false;
-        this.categoryData._id = params["cateID"];
-        this.getcategoryData();
-      } else {
-        this.isEdit = true;
-      }
-    });
-    this.getDataProduct();
-    // this.saveName == this._formBuilder.group({
-    //     name: ['', Validators.required]
-    // });
-  }
-  async getDataProduct() {
-    this.spinner.show();
-
-    try {
-      let user: any = JSON.parse(
-        window.localStorage.getItem(Constants.URL() + "@usershop")
-      );
-      this.page.shop_id = user.shop_id;
-      let respone: any = await this.restApi.post(
-        Constants.URL() + "/api/product-shop-list",
-        this.page
-      );
-      this.spinner.hide();
-
-      this.getProduct = respone.data.products;
-    } catch (error) {
-      this.spinner.hide();
+    categoryData: any = {
+        name: "กำหนดชื่อหมวดหมู่เอง",
+        items: [],
+        status: false
+    };
+    categoryCompele: boolean = false;
+    isCheckbtnSaveProduct: boolean = false;
+    itemSeleted: Array<any> = [];
+    getProduct: Array<any> = [];
+    isEdit: boolean = false;
+    page: any = {
+        shop_id: "",
+        status: "all",
+        name: "",
+        page: 1,
+        limit: 30
+    };
+    _formBuilder: any;
+    constructor(
+        public iconRegistry: MatIconRegistry,
+        public sanitizer: DomSanitizer,
+        private restApi: RestApiService,
+        private actRoute: ActivatedRoute,
+        private spinner: NgxSpinnerService,
+        private router: Router
+    ) {
+        iconRegistry.addSvgIcon(
+            "done",
+            sanitizer.bypassSecurityTrustResourceUrl("assets/baseline-done-24px.svg")
+        );
     }
-  }
-  onSearch(event) {
-    try {
-      if (event.key === "Enter") {
+
+    ngOnInit() {
+        this.actRoute.queryParams.subscribe(params => {
+            console.log(params);
+            if (params["cateID"]) {
+                this.isEdit = false;
+                this.categoryData._id = params["cateID"];
+                this.getcategoryData();
+            } else {
+                this.isEdit = true;
+            }
+        });
         this.getDataProduct();
-      }
-    } catch (error) {}
-  }
-  selectedItem(item) {
-    // this.spinner.show();
-    try {
-      item.product_id = item._id;
-      this.categoryData.items.push(item);
-      // this.spinner.hide();
-    } catch (error) {
-      // this.spinner.hide();
+        // this.saveName == this._formBuilder.group({
+        //     name: ['', Validators.required]
+        // });
     }
-  }
-  deleteItem(i, item) {
-    try {
-      this.categoryData.items.splice(i, 1);
-      if (this.categoryData.items && this.categoryData.items.length === 0) {
-        this.categoryCompele = false;
-      }
-    } catch (error) {}
-  }
-  checkButtonAdd(item) {
-    try {
-      let index = this.categoryData.items.findIndex(itm => {
-        return itm._id === item._id;
-      });
+    async getDataProduct() {
+        this.spinner.show();
 
-      if (index !== -1) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (error) {}
-  }
-  async onSaveData() {
-    try {
-      if (
-        this.categoryData.name &&
-        this.categoryData.items &&
-        this.categoryData.items.length === 0
-      ) {
-        this.categoryData.status = false;
-      }
+        try {
+            let user: any = JSON.parse(
+                window.localStorage.getItem(Constants.URL() + "@usershop")
+            );
+            this.page.shop_id = user.shop_id;
+            let respone: any = await this.restApi.post(
+                Constants.URL() + "/api/product-shop-list",
+                this.page
+            );
+            this.spinner.hide();
 
-      if (this.categoryData._id) {
-        this.categoryData.shop_id = this.page.shop_id;
-        let respone: any = await this.restApi.put(
-          Constants.URL() + "/api/categoryShop/" + this.categoryData._id,
-          this.categoryData
-        );
-        this.categoryData = respone.data;
-        this.getcategoryData();
-
-        if (
-          this.categoryData.name &&
-          this.categoryData.items &&
-          this.categoryData.items.length > 0
-        ) {
-          this.categoryCompele = true;
+            this.getProduct = respone.data.products;
+        } catch (error) {
+            this.spinner.hide();
         }
-      } else {
-        this.categoryData.shop_id = this.page.shop_id;
-        let respone: any = await this.restApi.post(
-          Constants.URL() + "/api/categoryShop/",
-          this.categoryData
-        );
-        this.categoryData = respone.data;
-        this.getcategoryData();
-        if (
-          this.categoryData.name &&
-          this.categoryData.items &&
-          this.categoryData.items.length > 0
-        ) {
-          this.categoryCompele = true;
+    }
+    onSearch(event) {
+        try {
+            if (event.key === "Enter") {
+                this.getDataProduct();
+            }
+        } catch (error) { }
+    }
+    selectedItem(item) {
+        // this.spinner.show();
+        try {
+            item.product_id = item._id;
+            this.categoryData.items.push(item);
+            this.categoryCompele = false;
+
+            // this.spinner.hide();
+        } catch (error) {
+            // this.spinner.hide();
         }
-      }
-      this.isEdit = false;
-    } catch (error) {}
-  }
-  async getcategoryData() {
-    try {
-      var resp: any = await this.restApi.get(
-        Constants.URL() + "/api/categoryShop/" + this.categoryData._id
-      );
-      this.categoryData = resp.data;
-      this.itemSeleted = JSON.parse(JSON.stringify(this.categoryData.items));
-    } catch (error) {}
-  }
-  onBack() {
-    this.router.navigate(["/shop-category"]);
-  }
+    }
+    deleteItem(i, item) {
+        try {
+            this.categoryData.items.splice(i, 1);
+            this.categoryCompele = false;
 
-  async onUpdateOpenCategory() {
-    this.categoryData.status = true;
-    await this.onSaveData();
-  }
+            if(!this.categoryData.name || (this.categoryData.items && this.categoryData.items.length === 0)){
+                this.categoryData.status = false;
+            }
+        } catch (error) { }
+    }
+    checkButtonAdd(item) {
+        try {
+            let index = this.categoryData.items.findIndex(itm => {
+                return itm._id === item._id;
+            });
 
-  async onChangeStatus() {
-    await this.onSaveData();
-  }
+            if (index !== -1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) { }
+    }
+    async onSaveData() {
+        this.spinner.show();
+        try {
+            if (
+                this.categoryData.name &&
+                this.categoryData.items &&
+                this.categoryData.items.length === 0
+            ) {
+                this.categoryData.status = false;
+                this.spinner.hide();
+            }
 
-  checkLogProductChange() {
-    let isRetrun = false;
+            if (this.categoryData._id) {
+                this.categoryData.shop_id = this.page.shop_id;
+                let respone: any = await this.restApi.put(
+                    Constants.URL() + "/api/categoryShop/" + this.categoryData._id,
+                    this.categoryData
+                );
+                this.categoryData = respone.data;
+                this.spinner.hide();
+                this.getcategoryData();
 
-    // console.log(this.categoryData.items);
+                if (
+                    this.categoryData.name &&
+                    this.categoryData.items &&
+                    this.categoryData.items.length > 0
+                ) {
+                    this.categoryCompele = true;
+                    this.spinner.hide();
 
-    // if (this.categoryCompele) {
-    //   isRetrun = false;
-    // } else {
-    //   isRetrun = true;
-    // }
-    // console.log(this.itemSeleted);
-    this.itemSeleted.forEach(item => {
-      let index = this.categoryData.items.findIndex(cateItem => {
-        return item.product_id === cateItem.product_id;
-      });
-      //   console.log(this.categoryData.items)
-      if (index === -1 || this.categoryData.items.length === 0) {
-        isRetrun = true;
-      }
-    });
+                }
+            } else {
+                this.categoryData.shop_id = this.page.shop_id;
+                let respone: any = await this.restApi.post(
+                    Constants.URL() + "/api/categoryShop/",
+                    this.categoryData
+                );
+                this.categoryData = respone.data;
+                this.spinner.hide();
 
-    this.categoryData.items.forEach(item => {
-      let index = this.itemSeleted.findIndex(cateItem => {
-        return item.product_id === cateItem.product_id;
-      });
+                this.getcategoryData();
+                if (
+                    this.categoryData.name &&
+                    this.categoryData.items &&
+                    this.categoryData.items.length > 0
+                ) {
+                    this.categoryCompele = true;
+                    this.spinner.hide();
 
-      if (index === -1 || this.itemSeleted.length === 0) {
-        isRetrun = true;
-      }
-    });
+                }
+            }
+            this.isEdit = false;
+            this.spinner.hide();
 
-    return isRetrun;
-  }
+        } catch (error) {
+            this.spinner.hide();
 
-  onRollbackItems() {
-    this.categoryData.items = JSON.parse(JSON.stringify(this.itemSeleted));
-  }
+        }
+
+    }
+    async getcategoryData() {
+
+        try {
+            var resp: any = await this.restApi.get(
+                Constants.URL() + "/api/categoryShop/" + this.categoryData._id
+            );
+
+            this.categoryData = resp.data;
+            this.itemSeleted = JSON.parse(JSON.stringify(this.categoryData.items));
+        } catch (error) { }
+    }
+    onBack() {
+        this.router.navigate(["/shop-category"]);
+    }
+
+    async onUpdateOpenCategory() {
+        this.categoryData.status = true;
+        await this.onSaveData();
+    }
+
+    async onChangeStatus() {
+        await this.onSaveData();
+    }
+
+    checkLogProductChange() {
+        let isRetrun = false;
+
+        // console.log(this.categoryData.items);
+
+        // if (this.categoryCompele) {
+        //   isRetrun = false;
+        // } else {
+        //   isRetrun = true;
+        // }
+        // console.log(this.itemSeleted);
+        this.itemSeleted.forEach(item => {
+            let index = this.categoryData.items.findIndex(cateItem => {
+                return item.product_id === cateItem.product_id;
+            });
+            //   console.log(this.categoryData.items)
+            if (index === -1 || this.categoryData.items.length === 0) {
+                isRetrun = true;
+            }
+        });
+
+        this.categoryData.items.forEach(item => {
+            let index = this.itemSeleted.findIndex(cateItem => {
+                return item.product_id === cateItem.product_id;
+            });
+
+            if (index === -1 || this.itemSeleted.length === 0) {
+                isRetrun = true;
+            }
+        });
+
+        return isRetrun;
+    }
+
+    onRollbackItems() {
+        this.categoryData.items = JSON.parse(JSON.stringify(this.itemSeleted));
+    }
 }
