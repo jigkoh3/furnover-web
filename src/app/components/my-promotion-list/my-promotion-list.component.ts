17ea8e4fd141378c53d7315889b5d907b5633865
Joseboom <boom.jaidee@hotmail.com>
2018-10-10 16:58:55 +0700

    edit

--- src/app/components/my-promotion-list/my-promotion-list.component.ts
+++ src/app/components/my-promotion-list/my-promotion-list.component.ts
@@ -17,10 +17,10 @@ export class MyPromotionListComponent implements OnInit {
 
   constructor(
     public iconRegistry: MatIconRegistry,
-    public sanitizer: DomSanitizer ) {
-      iconRegistry.addSvgIcon(
-        'del',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/imgs/icons/del.svg'));
+    public sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'del',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/imgs/icons/del.svg'));
   }
 
   ngOnInit() {
@@ -34,6 +34,12 @@ export class MyPromotionListComponent implements OnInit {
           limitsell: '0',
           img: 'https://doublegoose.com/wp-content/uploads/2014/03/doublegoose-crewneck-333n-bl.png',
           prices: [{
+            oldprice: '130',
+            newprice: '100',
+            discount: '10',
+            choice: true,
+            namechoice: 'เสื้อสีดำ'
+          }, {
             oldprice: '130',
             newprice: '100',
             discount: '10',
