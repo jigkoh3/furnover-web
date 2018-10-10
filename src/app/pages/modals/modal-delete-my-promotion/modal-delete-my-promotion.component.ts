94192623691da364212a1ff066ddff6a91e4f8b3
mob35 <toonelicious@gmail.com>
2018-10-10 17:40:48 +0700

    promo

--- src/app/pages/modals/modal-delete-my-promotion/modal-delete-my-promotion.component.ts
+++ src/app/pages/modals/modal-delete-my-promotion/modal-delete-my-promotion.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { RestApiService } from '../../../providers/rest-api-service/rest-api.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { DataService } from '../../../providers/data-service/data.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Constants } from '../../../app.constants';
 
 @Component({
   selector: 'app-modal-delete-my-promotion',
   templateUrl: './modal-delete-my-promotion.component.html',
-  styleUrls: ['./modal-delete-my-promotion.component.css']
+  styleUrls: ['./modal-delete-my-promotion.component.scss']
 })
 export class ModalDeleteMyPromotionComponent implements OnInit {
+  data: any = {}
+  constructor(
+    private restApi: RestApiService,
+    private spinner: NgxSpinnerService,
+    private dataService: DataService,
+    public dialogRef: MatDialogRef<ModalDeleteMyPromotionComponent>,
+    @Inject(MAT_DIALOG_DATA) public item: any
+  ) {
 
-  constructor() { }
+    this.data = item;
+    console.log(this.data)
+  }
 
   ngOnInit() {
   }
-
+  async delete() {
+    this.spinner.show();
+    try {
+      let res: any = await this.restApi.post(Constants.URL() + '/api/xxx-delete/', this.data);
+      if (res['status'] === 200) {
+        this.dialogRef.close(res);
+        this.spinner.hide();
+      }
+    } catch (errer) {
+      this.spinner.hide();
+      this.dataService.error('ลบโปรโมชั่นล้มเหลว กรุณาลองอีกครั้ง');
+      setTimeout(() => {
+        this.dataService.error('');
+      }, 2000);
+    }
+  }
 }
