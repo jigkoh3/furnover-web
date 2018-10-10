bcf68c81f3303182def7eb8d7d3d812edc102598
Joseboom <boom.jaidee@hotmail.com>
2018-10-10 16:52:40 +0700

    edit

--- src/app/components/my-promotion-list/my-promotion-list.component.ts
+++ src/app/components/my-promotion-list/my-promotion-list.component.ts
@@ -18,10 +18,9 @@ export class MyPromotionListComponent implements OnInit {
   constructor(
     public iconRegistry: MatIconRegistry,
     public sanitizer: DomSanitizer ) {
-    iconRegistry.addSvgIcon(
-      "cancel",
-      sanitizer.bypassSecurityTrustResourceUrl("assets/baseline-cancel-24px.svg")
-    );
+      iconRegistry.addSvgIcon(
+        'del',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/imgs/icons/del.svg'));
   }
 
   ngOnInit() {
@@ -36,31 +35,6 @@ export class MyPromotionListComponent implements OnInit {
           img: 'https://doublegoose.com/wp-content/uploads/2014/03/doublegoose-crewneck-333n-bl.png',
           prices: [{
             oldprice: '130',
-            newprice: '100',
-            discount: '10',
-            choice: true,
-            namechoice: 'เสื้อสีดำ'
-          },
-          {
-            oldprice: '120',
-            newprice: '100',
-            discount: '10',
-            choice: true,
-            namechoice: 'เสื้อสีดำ'
-          }]
-        }, {
-          name: 'กางเกง',
-          limitsell: '0',
-          img: 'https://doublegoose.com/wp-content/uploads/2014/03/doublegoose-crewneck-333n-bl.png',
-          prices: [{
-            oldprice: '130',
-            newprice: '100',
-            discount: '10',
-            choice: true,
-            namechoice: 'เสื้อสีดำ'
-          },
-          {
-            oldprice: '120',
             newprice: '100',
             discount: '10',
             choice: true,
