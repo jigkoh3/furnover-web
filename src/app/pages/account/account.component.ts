075af792ce2af60bedbf58f88ee061dca6e0d8d6
niDniDD <oraphin.poo@northbkk.ac.th>
2018-10-10 17:27:32 +0700

    [showh] account

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import * as _moment from 'moment';
+const moment = _moment;
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class AccountComponent implements OnInit {
+
+  date = new Date();
 
   constructor() { }
 
