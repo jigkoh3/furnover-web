a66c1db45c248662e3a447cb5e73b5d656bcf78a
pure1234567a <pure1234567a@gmail.com>
2018-10-10 14:44:52 +0700

    p

--- src/app/components/my-promotion-header/my-promotion-header.component.ts
+++ src/app/components/my-promotion-header/my-promotion-header.component.ts
@@ -7,25 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-promotion-header.component.css']
 })
 export class MyPromotionHeaderComponent implements OnInit {
-
+  myDate = new Date();
   constructor(
-    
+
   ) { }
 
   ngOnInit() {
+    this.getDataTest();
   }
 
-  // getDataTest(){
-  //   let res = {
-  //     status : "200",
-  //     data : {
-  //       title : "เทสส่วนลด",
-  //       start_date : "1/5/16",
-  //       end_date : "1/5/16",
-  //       start_time : "9:05 AM",
-  //       end_time : "9:05 PM"
-  //     }
-  //   }
-  // }
+  getDataTest() {
+    let res = {
+      status: "200",
+      data: {
+        title: "เทสส่วนลด",
+        start_date: "1/5/16",
+        end_date: "1/5/16",
+        start_time: "9:05 AM",
+        end_time: "9:05 PM"
+      }
+    }
+  }
 
 }
