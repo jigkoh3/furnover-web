dc787bdd2cfa230ee722ff495097be319207775f
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-10-10 11:59:07 +0700

    update

--- src/app/pages/modals/modal-select-product/modal-select-product.component.ts
+++ src/app/pages/modals/modal-select-product/modal-select-product.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 
 @Component({
-  selector: 'app-modal-select-product',
-  templateUrl: './modal-select-product.component.html',
-  styleUrls: ['./modal-select-product.component.css']
+  selector: "app-modal-select-product",
+  templateUrl: "./modal-select-product.component.html",
+  styleUrls: ["./modal-select-product.component.css"]
 })
 export class ModalSelectProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<ModalSelectProductComponent>
+  ) {
+    console.log(data);
   }
 
+  ngOnInit() {}
+
+  closeModel() {
+    this.dialogRef.close();
+  }
 }
