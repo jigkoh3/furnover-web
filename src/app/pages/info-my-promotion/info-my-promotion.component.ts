4f4bb34cba236124d63304fc0e8c1e73368cabd8
kanomdook <kanung.2535@gmail.com>
2018-10-11 14:48:37 +0700

    [add] fn call service  /api/discount

--- src/app/pages/info-my-promotion/info-my-promotion.component.ts
+++ src/app/pages/info-my-promotion/info-my-promotion.component.ts
@@ -16,6 +16,10 @@ export class InfoMyPromotionComponent implements OnInit {
   data: any = {
     products: []
   };
+  oldTime: any = {
+    starttime: '00:00',
+    endtime: '00:00'
+  };
   // data: any = {
   //   _id: '1234',
   //   shop_id: '001',
@@ -91,16 +95,36 @@ export class InfoMyPromotionComponent implements OnInit {
   }
 
   async save() {
-    console.log(this.data);
-    // this.spinner.show();
-    // try {
-    //   const res: any = await this.restApi.post(Constants + '/api/discount', this.data);
-    //   console.log(res);
-    //   this.spinner.hide();
-    // } catch (error) {
-    //   console.log(error);
-    //   this.spinner.hide();
-    // }
+    this.spinner.show();
+    this.oldTime = {
+      starttime: this.data.starttime,
+      endtime: this.data.endtime
+    };
+    const dateTimeStart = new Date(this.data.startdate);
+    const timespl = this.data.starttime.split(':');
+    dateTimeStart.setHours(timespl[0], timespl[1], 0, 0);
+    const dateTimeEnd = new Date(this.data.enddate);
+    const timespl2 = this.data.endtime.split(':');
+    dateTimeEnd.setHours(timespl2[0], timespl2[1], 0, 0);
+    this.data.startdate = dateTimeStart;
+    this.data.enddate = dateTimeEnd;
+    this.data.starttime = dateTimeStart;
+    this.data.endtime = dateTimeEnd;
+    const userShop: any = JSON.parse(window.localStorage.getItem(Constants.URL() + '@usershop'));
+    this.data.shop_id = userShop.shop._id;
+
+    try {
+      const res: any = await this.restApi.post(Constants.URL() + '/api/discount', this.data);
+      console.log(res);
+      this.data.starttime = this.oldTime.starttime;
+      this.data.endtime = this.oldTime.endtime;
+      this.spinner.hide();
+    } catch (error) {
+      console.log(error);
+      this.data.starttime = this.oldTime.starttime;
+      this.data.endtime = this.oldTime.endtime;
+      this.spinner.hide();
+    }
   }
 
   async delete() {
@@ -108,7 +132,7 @@ export class InfoMyPromotionComponent implements OnInit {
     if (confirm) {
       this.spinner.show();
       try {
-        const res: any = await this.restApi.delete(Constants + '/api/discount/' + this.data._id);
+        const res: any = await this.restApi.delete(Constants.URL() + '/api/discount/' + this.data._id);
         console.log(res);
         this.spinner.hide();
       } catch (error) {
