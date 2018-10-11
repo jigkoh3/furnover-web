e82457a2d5f234dbe01786bc16d292d182b2167c
pure1234567a <pure1234567a@gmail.com>
2018-10-11 16:55:03 +0700

    save data info-my-code

--- src/app/pages/info-my-code/info-my-code.component.ts
+++ src/app/pages/info-my-code/info-my-code.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import * as _moment from 'moment';
+import { MatDialog } from '@angular/material';
+import { ModalSelectProductComponent } from '../modals/modal-select-product/modal-select-product.component';
+import { ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { RestApiService } from 'src/app/providers/rest-api-service/rest-api.service';
+import { Constants } from 'src/app/app.constants';
 const moment = _moment;
 export const MY_FORMATS = {
   parse: {
@@ -28,7 +34,7 @@ export const MY_FORMATS = {
 export class InfoMyCodeComponent implements OnInit {
 
   data: any = {
-  
+
     cash: {
       discount: 0,
       minprice: 0,
@@ -38,24 +44,48 @@ export class InfoMyCodeComponent implements OnInit {
       percentagetype: "limit", // มีสองอย่าง limit กับ unlimit
       setamount: 0,
       minprice: 0
-    }
+    },
+    products: []
   };
   date = new Date();
   _startdate: any;
   _enddate: any;
+  itemId: any;
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private activatedRoute: ActivatedRoute,
+    private spinner: NgxSpinnerService,
+    private restApi: RestApiService
+  ) { }
 
   ngOnInit() {
-    this.getDataTest();
-    // console.log(this.data);
+    this.activatedRoute
+      .queryParams
+      .subscribe(params => {
+        if (params['itemId']) {
+          this.itemId = params['itemId'];
+          this.initLoadData();
+        }
+      });
   }
 
-  getDataTest() {
-    
-    //this.data = res.data;
+  async initLoadData() {
+    this.spinner.show();
+    try {
+      const res: any = await this.restApi.get(Constants.URL() + '/api/mycode/' + this.itemId);
+      if(res.data._id){
+        this.data = res.data;
+      }
+      
+      this.spinner.hide();
+    } catch (error) {
+      console.log(error);
+      this.spinner.hide();
+    }
   }
-  isChecked(){
+
+  isChecked() {
     return this.data.itemtype == 'all';
   }
   startDate(e) {
@@ -76,6 +106,73 @@ export class InfoMyCodeComponent implements OnInit {
 
   saveDate() {
     console.log(this.data)
+  }
+
+  openModalAddProduct() {
+    const dialogRef = this.dialog.open(ModalSelectProductComponent, {
+      width: '95vw',
+      height: '90vh',
+      data: { products: this.data.products.length > 0 ? this.data.products : [], status: 'sell' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        result.forEach(item => {
+          const index = this.data.products.findIndex(findindex => {
+            return findindex._id === item._id;
+          });
+          if (index === -1 || this.data.products.length === 0) {
+            this.data.products.push(item);
+          }
+        });
+      }
+    });
+  }
+
+  
+
+  async saveData() {
+    this.spinner.show();
+    // this.oldTime = {
+    //   starttime: this.data.starttime,
+    //   endtime: this.data.endtime
+    // };
+    // const dateTimeStart = new Date(this.data.startdate);
+    // const timespl = this.data.starttime.split(':');
+    // dateTimeStart.setHours(timespl[0], timespl[1], 0, 0);
+    // const dateTimeEnd = new Date(this.data.enddate);
+    // const timespl2 = this.data.endtime.split(':');
+    // dateTimeEnd.setHours(timespl2[0], timespl2[1], 0, 0);
+    // this.data.startdate = dateTimeStart;
+    // this.data.enddate = dateTimeEnd;
+    // this.data.starttime = dateTimeStart;
+    // this.data.endtime = dateTimeEnd;
+    const userShop: any = JSON.parse(window.localStorage.getItem(Constants.URL() + '@usershop'));
+    this.data.shop_id = userShop.shop._id;
+
+    if (this.itemId) {
+      try {
+        const res: any = await this.restApi.put(Constants.URL() + '/api/Mycode/' + this.itemId, this.data);
+        
+        this.spinner.hide();
+        
+      } catch (error) {
+        console.log(error);
+        
+        this.spinner.hide();
+      }
+    } else {
+      try {
+        const res: any = await this.restApi.post(Constants.URL() + '/api/Mycode', this.data);
+       
+        this.spinner.hide();
+        
+      } catch (error) {
+        console.log(error);
+       
+        this.spinner.hide();
+      }
+    }
   }
 
 }
