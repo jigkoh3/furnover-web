ef9a1f7f02c055b8c24c2acf1d5124cd9670ba63
kanomdook <kanung.2535@gmail.com>
2018-11-13 16:14:23 +0700

    [edit] height conversation box

--- src/app/components/ng-chat/ng-chat.component.ts
+++ src/app/components/ng-chat/ng-chat.component.ts
@@ -43,6 +43,7 @@ export class NgChatComponent implements OnInit {
         _id: '2',
         img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
       },
+      // tslint:disable-next-line:max-line-length
       chat: '** Samsung สวัสดีค่ะ สอบถามสินค้า หรือบริการ กรุณารอสักครู่ .. เนื่องจากมีผู้สอบถามเข้ามาจำนวนมาก เจ้าหน้าที่จะรีบติดต่อกลับโดยเร็ว ขอบคุณค่ะ... THANK YOU!! (สินค้าน้ำหนักเกิน 30 กิโลขึ้นไป กรุงเทพฯ และ ปริมณฑล ส่งฟรี!! )...',
       dateTime: 'วันนี้, 13:00 น.'
     },
