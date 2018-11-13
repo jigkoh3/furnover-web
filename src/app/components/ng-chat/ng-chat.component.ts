f31a7d6522d5e79987d3fce823561e7f3d440694
kanomdook <kanung.2535@gmail.com>
2018-11-13 15:51:26 +0700

    [add] chat bubble

--- src/app/components/ng-chat/ng-chat.component.ts
+++ src/app/components/ng-chat/ng-chat.component.ts
@@ -8,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class NgChatComponent implements OnInit {
   isMinimize = true;
   person = 0;
+  chat: string;
+  conversationList: Array<any> = [];
+  receiver = '';
   constructor() { }
 
   ngOnInit() {
+    // setTimeout(() => {
+    //   const element = document.getElementById('box');
+    //   element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    // }, 400);
+
+    this.conversationList = [{
+      _id: '11111', // ลูกค้า
+      user: {
+        _id: '1',
+        img: 'https://www.pngarts.com/files/3/Avatar-Transparent-Image.png'
+      },
+      chat: 'สวัสดีครับ มีของหรือเปล่าครับ',
+      dateTime: 'วันนี้, 11:30 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
+    }];
+
+    this.receiver = '1';
   }
 
   minimize() {
