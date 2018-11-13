eae3952930cd93150c5bebf78afaf251e0a36edd
kanomdook <kanung.2535@gmail.com>
2018-11-13 16:01:10 +0700

    [add] conversation list

--- src/app/components/ng-chat/ng-chat.component.ts
+++ src/app/components/ng-chat/ng-chat.component.ts
@@ -36,6 +36,87 @@ export class NgChatComponent implements OnInit {
       },
       chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
       dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: '** Samsung สวัสดีค่ะ สอบถามสินค้า หรือบริการ กรุณารอสักครู่ .. เนื่องจากมีผู้สอบถามเข้ามาจำนวนมาก เจ้าหน้าที่จะรีบติดต่อกลับโดยเร็ว ขอบคุณค่ะ... THANK YOU!! (สินค้าน้ำหนักเกิน 30 กิโลขึ้นไป กรุงเทพฯ และ ปริมณฑล ส่งฟรี!! )...',
+      dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
+    },
+    {
+      _id: '22222', // เจ้าของร้าน
+      user: {
+        _id: '2',
+        img: 'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'
+      },
+      chat: 'สวัสดีค่ะ มีสินค้าค่ะ',
+      dateTime: 'วันนี้, 13:00 น.'
     }];
 
     this.receiver = '1';
