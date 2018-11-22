cbeb6ffc29d2810d72a520cc9bdc72262895fc7c
pure1234567a <pure1234567a@gmail.com>
2018-11-22 10:30:09 +0700

    p

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -53,22 +53,28 @@ export class AccountComponent implements OnInit {
   clickShow1() {
     if (this.show1) {
       this.show1 = false;
+      this.getData();
     } else {
       this.show1 = true;
+      this.getData();
     }
   }
   clickShow2() {
     if (this.show2) {
       this.show2 = false;
+      this.getData();
     } else {
       this.show2 = true;
+      this.getData();
     }
   }
   clickShow3() {
     if (this.show3) {
       this.show3 = false;
+      this.getData();
     } else {
       this.show3 = true;
+      this.getData();
     }
   }
 
