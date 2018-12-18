5a0c1a1182b406dd555452e8d549542e8f0caad3
kanomdook <kanung.2535@gmail.com>
2018-12-18 17:59:36 +0700

    [edit] api

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
-    public static URL() { return 'https://furnover-api-test.herokuapp.com'; } // Dev
+    public static URL() { return 'https://furnover-api-dev.herokuapp.com'; } // Dev
     // public static URL() { return 'http://localhost:3000'; } // Local
 }
