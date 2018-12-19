d444eb2ae3d88cc5f68cf017a61cdfca5784de2c
Joseboom <boom.jaidee@hotmail.com>
2018-12-19 11:22:17 +0700

    build

--- src/app/pages/my-sale/my-sale.component.ts
+++ src/app/pages/my-sale/my-sale.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { ModalConfirmRemarkComponent } from '../../modals/modal-confirm-remark/modal-confirm-remark.component';
+import { OrderService } from 'casan-order-test';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ModalCompleteComponent } from '../modals/modal-complete/modal-complete.component';
+
 
 @Component({
   selector: 'app-my-sale',
@@ -33,7 +39,46 @@ export class MySaleComponent implements OnInit {
     }
   ]
 
-  constructor(public route: Router) { }
+  constructor(
+    public route: Router,
+    private orderService: OrderService,
+    private dialog: MatDialog,
+    private spinner: NgxSpinnerService,
+  ) {
+    this.orderService.loadData.subscribe(() => {
+      this.spinner.show();
+    });
+    this.orderService.loadDataSuccess.subscribe(() => {
+      this.spinner.hide();
+    });
+    this.orderService.openingModalWaitshipping.observers = [];
+    this.orderService.openingModalWaitshipping.subscribe((order_id) => {
+      console.log(order_id);
+      const dialogRef = this.dialog.open(ModalConfirmRemarkComponent, {
+        width: "500px",
+        data: { message: "ยืนยันการจัดส่ง?" },
+        autoFocus: false
+      });
+      dialogRef.afterClosed().subscribe(async result => {
+        console.log(result);
+        console.log(`Dialog closed: ${result}`);
+        const resultConfrim = result;
+        if (resultConfrim === "confirm") {
+          console.log('confirm');
+          this.orderService.openModalWaitshippingConfirm.emit(order_id);
+          // console.log("object");
+        }
+      });
+    });
+    this.orderService.updateStatusCompelete.observers = [];
+    this.orderService.updateStatusCompelete.subscribe(() => {
+      const dialogRef = this.dialog.open(ModalCompleteComponent, {
+        width: '500px',
+        data: { message: 'ทำรายการสำเร็จ' },
+        autoFocus: false
+      });
+    });
+  }
 
   ngOnInit() {
   }
