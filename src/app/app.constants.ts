74685104d4dffcd1c27af093fef9b3c2be77b659
kanomdook <kanung.2535@gmail.com>
2018-12-25 13:52:03 +0700

    [build] Dev

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    // public static URL() { return 'https://furnover-api-dev.herokuapp.com'; } // Dev
-    public static URL() { return 'https://furnover-api-test.herokuapp.com'; } // Test
+    public static URL() { return 'https://furnover-api-dev.herokuapp.com'; } // Dev
+    // public static URL() { return 'https://furnover-api-test.herokuapp.com'; } // Test
     // public static URL() { return 'http://localhost:3000'; } // Local
 }
